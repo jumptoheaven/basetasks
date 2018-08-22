9bd140d825c96c6143119badfd52c72831d5b012
Sergey <yurichsergey@gmail.com>
2018-08-22 23:15:42 +0300

    Added and tested Selection sort
    fix find minimal element in Selection Sort Class

--- classes/SortSelected.php
+++ classes/SortSelected.php
@@ -3,37 +3,32 @@
 
 class SortSelected implements SortableInterface
 {
-	public function sort(
-		array $a,
-		$descendDirect = false
-	): array
-	{
-		$countEls = count($a);
-		$countIter = $countEls - 1;
-		$rangeEls = $countEls - 1;
-		$lastEl = $countEls - 1;
-		for (
-			$iter = 0;
-			$iter < $countIter;
-			$iter++
-		) {
-			//pr('Main iter #'.$iter);
-		   $indexMinimalEl = $this->findIndexMinimalEl($a, $startIndex, $stopIndex);
+    public function sort(array $a, $descendDirect = false): array
+    {
+        $countEls = count($a);
+        $countIter = $countEls - 1;
+        $startIndex = 0;
+        $stopIndex = $countEls - 1;
+        for (
+            $iter = 0;
+            $iter < $countIter;
+            $iter++
+        ) {
+            // pr('Main iter #'.$iter.' ['.$startIndex.'-'. $stopIndex.']  -|-->  '.json_encode($a));
 
-				$isChangePos = (
-						$descendDirect && $isAscend
-					) || (
-						(!$descendDirect) && (!$isAscend)
-					);
-				if ($isChangePos) {
-					$this->changePosEls(
-							$a, $i, $i -1
-						);
-				}
+            $indexMinimalEl = $this->findIndexMinimalEl($a, $startIndex, $stopIndex);
 
-		}
-		return $a;
-	}
+            $isChangePos = ($indexMinimalEl !== $startIndex);
+            if ($isChangePos) {
+                $this->changePosEls($a, $startIndex, $indexMinimalEl);
+            }
+
+            // pr('   ['.$startIndex.'-'. $stopIndex.'] -- ' . json_encode([ $isChangePos, $startIndex, $indexMinimalEl]) . ' -|-->  ' . json_encode($a)   );
+
+            $startIndex ++;
+        }
+        return $a;
+    }
 
     /**
      * Indexes start from 0 to (count - 1)
@@ -45,7 +40,7 @@ class SortSelected implements SortableInterface
         $res = $startIndex;
         for ($i = $startIndex; $i < $stopIndex; $i++) {
             $i2 = $i + 1;
-            if ($arr[$i2] < $arr[$i]) {
+            if ($arr[$i2] < $arr[$res]) {
                 $res = $i2;
             }
         }
--- classes/TestSortable.php
+++ classes/TestSortable.php
@@ -3,32 +3,25 @@
 
 class TestSortable
 {
-    public function testCases(SortableInterface $sorter)
+     public function testCases(SortableInterface $sorter)
     {
-	    $cases = [
-    		['a'=>[1,2,4,5,7,],
-	    		'b'=>[1,2,4,5,7,],],
-	    	['a'=>[5,2,1,4,7,],
-		    	'b'=>[1,2,4,5,7,],],
-    		['a'=>[7,5,4,2,1,],
-			    'b'=>[1,2,4,5,7,],],
-    	];
-    	foreach ($cases as $testCase) {
-	    	$testData = $testCase['a'];
-    		$answer = $sorter
-	        		->sort($testData);
-    		$answStr = json_encode($answer);
+        $cases = [
+            ['a'=>[1,2,4,5,7,], 'b'=>[1,2,4,5,7,],],
+            ['a'=>[5,2,1,4,7,], 'b'=>[1,2,4,5,7,],],
+            ['a'=>[7,5,4,2,1,], 'b'=>[1,2,4,5,7,],],
+            ['a'=>[7,97,4,3,0,], 'b'=>[0,3,4,7,97,],],
+        ];
+        foreach ($cases as $testCase) {
+            $testData = $testCase['a'];
+            $answer = $sorter->sort($testData);
+            $assert = $testCase['b'];
 
-    		$assert = $testCase['b'];
-	    	$assertStr = json_encode($assert);
-
-	    	$r = ($answer == $assert) ?
-	    		'SUCCESSES' : 'ERROR';
-	    	$msg = $r . ' [answ/assert]: '
-	    		. $answStr . ' / '
-    			. $assertStr;
-		    pr($msg);
-	    }
+            $r = ($answer == $assert) ? 'OK' : 'ERR';
+            $msg = $r . '  [in/res/ass]: '
+                . json_encode($testData) . ' / '
+                . json_encode($answer) . ' / '
+                . json_encode($assert);
+            pr($msg);
+        }
     }
 }
-
